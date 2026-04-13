0dca00eb7aac18f4e1e0a72b210bdba39c97ab09
storm309 <shivamkumarp447@gmail.com>
2026-04-13 11:08:02 +0530

    New programs Added

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/secure',function(){
+    if(request()->secure()){
+        return "This is a secure request";
+    }
+    return "Use HTTPS to access this route";
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -15,6 +23,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::domain('shivam.com')->group(function () {
+    Route::get('/', function () {
+        return "Welcome to the admin dashboard";
+    });
+    Route::get('/dashboard', function () {
+        return "Welcome to the admin dashboard";
+    });
 });
 
 require __DIR__.'/auth.php';
